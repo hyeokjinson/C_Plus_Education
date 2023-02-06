ad36994d4fd88f6b23dabd54df11bb356de046d8
hyek jin son <hjson817@mokpo.ac.kr>
2023-02-06 22:26:45 +0900

    fprintf

--- printf/printf/printf.cpp
+++ printf/printf/printf.cpp
@@ -1,11 +1,25 @@
 ﻿#include<cstdio>
+#include<cstdlib>
+#include <iostream>
+#include<fstream>
+#pragma warning(disable:4996)
 
+using namespace std;
 int main() {
+	FILE* fp;
+
+	fp = fopen("example.log", "w+");
+	fprintf(fp, "example code 1");
+	fclose(fp);
+
 	printf("Hello %d\n", 10);
 	printf("%08X\n", 255);
 
 	printf("%f-%f=%lf\n", 2.0, 3.0f, -1.0f);
 
-	printf("%x+%x=%X", 10, 10, 20);
+	printf("%x+%x=%X\n", 10, 10, 20);
 
+	//freopen("output.log", "w", stdout);
+	
+	printf("Hello World\n");
 }
