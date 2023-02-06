5798b37a7d2a26da40b6721fe6b8e8a65d47d4f2
hyek jin son <hjson817@mokpo.ac.kr>
2023-02-06 21:45:23 +0900

    string_Education

---
+++ string/string/string.cpp
@@ -0,0 +1,18 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	char ch = 'a';
+	char ch1 = 99;
+	cout << "ch1 : " << ch1 << endl;
+	cout << ch << endl;
+	cout << "ch1 : " << (int)ch1 << endl;
+	cout << (int)ch << endl;
+
+	cout << numeric_limits<uint8_t>::max() << endl;
+
+	cout << "%08x" << 255<<endl;
+}
+
